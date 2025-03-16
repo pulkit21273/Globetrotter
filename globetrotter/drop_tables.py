82a66f714ebaca956b93c40376840ea79973775f
pulkit21273 <pulkit21273@iiitd.ac.in>
2025-03-16 12:08:00 +0530

    minor_script

--- globetrotter/drop_tables.py
+++ globetrotter/drop_tables.py
@@ -4,8 +4,10 @@ from models.destination import Destination
 from models.clue import Clue  
 from models.funfact import FunFact  
 from models.trivia import Trivia 
+from sqlalchemy import text
 
 
-Base.metadata.drop_all(engine, tables=[User.__table__])
+Base.metadata.drop_all(engine)
 
-print("Tables dropped successfully")
+
+print("All tables dropped successfully")
