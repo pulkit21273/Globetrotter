8a0ddaf312e9b71f820b5277710129ab51fc482c
pulkit21273 <pulkit21273@iiitd.ac.in>
2025-04-03 18:07:52 +0530

    latest

--- globetrotter/config.py
+++ globetrotter/config.py
@@ -1,3 +1,3 @@
-DATABASE_URL = "postgresql://postgres:postgres@postgres.cl2weeu84f2v.ap-south-1.rds.amazonaws.com:5432/postgres"
-
+# DATABASE_URL = "postgresql://postgres:postgres@postgres.cl2weeu84f2v.ap-south-1.rds.amazonaws.com:5432/postgres"
+DATABASE_URL = "postgresql://postgres:postgres@globetrotter.c56cuue62vgc.ap-south-1.rds.amazonaws.com"
 # DATABASE_URL = "postgresql://postgres:postgres@localhost:5434/postgres"
